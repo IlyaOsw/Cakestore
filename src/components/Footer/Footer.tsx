b353678e7bc472f048d272fd1fff5cbfc6661178
IlyaOsw <ilya_osw@bk.ru>
2023-11-21 22:37:55 +0200

    Added email in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,36 +1,13 @@
 import React from "react";
 import "./Footer.scss";
-import { useTranslation } from "react-i18next";
 import Follow from "./Follow/Follow";
+import FooterContacts from "./FooterContacts/FooterContacts";
 
 const Footer: React.FC = () => {
-  const { t } = useTranslation();
   return (
     <div className="block-footer fs-4">
-      <div className="d-sm-flex justify-content-around footer-contact mb-2">
-        <div className="info z-3">
-          <img
-            src={process.env.PUBLIC_URL + `/images/Location.png`}
-            alt="Location"
-          />
-          <a
-            href="https://www.google.com/maps/place/%D0%A2%D0%B0%D1%80%D1%82%D1%83/@58.3752523,26.6503333,12z/data=!4m6!3m5!1s0x46eb36d8d55d3df7:0x400b36d18fc6d10!8m2!3d58.377983!4d26.7290383!16zL20vMDdzYjE?entry=ttu"
-            target="_blank"
-            className="line"
-          >
-            {t("location")}
-          </a>
-        </div>
-        <div className="info z-3 ms-1">
-          <img
-            src={process.env.PUBLIC_URL + `/images/Phone.png`}
-            alt="Phone"
-            className="mb-1"
-          />
-          <a href="tel:+37256268892" className="line phone_link">
-            +372-5626-8892
-          </a>
-        </div>
+      <div className="container d-sm-flex justify-content-around footer-contact mb-2">
+        <FooterContacts />
         <Follow />
       </div>
       <div className="footer-copyright pb-4 z-3">
---
+++ src/components/Footer/FooterContacts/FooterContacts.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "../Footer.scss";
+import { useTranslation } from "react-i18next";
+
+const FooterContacts: React.FC = () => {
+  const { t } = useTranslation();
+
+  enum Contacts {
+    LOCATION = "location",
+    PHONE = "phone",
+    MAIL = "mail",
+  }
+
+  const contacts: {
+    id: number;
+    label: Contacts;
+    name: string;
+    link: string;
+  }[] = [
+    {
+      id: 1,
+      label: Contacts.LOCATION,
+      name: "location",
+      link: "https://www.google.com/maps/place/%D0%A2%D0%B0%D1%80%D1%82%D1%83/@58.3752523,26.6503333,12z/data=!4m6!3m5!1s0x46eb36d8d55d3df7:0x400b36d18fc6d10!8m2!3d58.377983!4d26.7290383!16zL20vMDdzYjE?entry=ttu",
+    },
+    {
+      id: 2,
+      label: Contacts.PHONE,
+      name: "+372-5626-8892",
+      link: "tel:+37256268892",
+    },
+    {
+      id: 3,
+      label: Contacts.MAIL,
+      name: "annalevina@gmail.com",
+      link: "mailto: annalevina1803@gmail.com",
+    },
+  ];
+
+  const getContacts = () =>
+    contacts.map((el) => (
+      <div className="info z-3 ms-1" key={el.id}>
+        <img
+          src={process.env.PUBLIC_URL + `/images/${el.label}.png`}
+          alt={el.label}
+          className="mb-1 me-1"
+        />
+        <a href={el.link} className="line">
+          {t(el.name)}
+        </a>
+      </div>
+    ));
+  return <>{getContacts()}</>;
+};
+
+export default FooterContacts;
