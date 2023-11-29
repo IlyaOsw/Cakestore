6328c8d5b34804d7612a12981aa82784e8b9bcf8
IlyaOsw <ilya_osw@bk.ru>
2023-11-30 00:51:36 +0200

    Fixes

--- src/pages/Price/Delivery/Delivery.tsx
+++ src/pages/Price/Delivery/Delivery.tsx
@@ -5,24 +5,22 @@ import { useTranslation } from "react-i18next";
 const Delivery: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <>
-      <div className="information mt-5 hr">
-        <div className="fs-4 animation_item item">
-          <p className="fs-2 subtitle">{t("order")}</p>
-          <p>{t("myself")}</p>
-          <p>{t("deliveryPrice")}</p>
-        </div>
-        <div className="fs-4 red animation_item item">
-          <p>{t("PS")}</p>
-          <br />
-          <p>
-            {t("prepayment1")}
-            <br />
-            {t("prepayment2")}
-          </p>
-        </div>
+    <div className="information mt-5 hr">
+      <div className="fs-4 animation_item item">
+        <p className="fs-2 subtitle">{t("order")}</p>
+        <p>{t("myself")}</p>
+        <p>{t("deliveryPrice")}</p>
       </div>
-    </>
+      <div className="fs-4 red animation_item item">
+        <p>{t("PS")}</p>
+        <br />
+        <p>
+          {t("prepayment1")}
+          <br />
+          {t("prepayment2")}
+        </p>
+      </div>
+    </div>
   );
 };
 
